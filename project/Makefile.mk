ad1850cb233d03c4fdda433bb08e2abc82c91518
Matteo Destro <matteo.est@gmail.com>
2019-04-16 16:46:49 +0200

    Aggiunto gitignore

--- project/Makefile.mk
+++ project/Makefile.mk
@@ -1,7 +1,7 @@
 help:
 	@echo "HELP"
 build:
-	@gcc ./project/src/controller/main.c -std=gnu90 -o ./project/controller
+	@gcc ./project/src/controller/main.c -std=gnu90 -o ./project/bin/controller
 	#	TODO: gcc per ogni dispositivo
 clean:
 	@echo "TODO"
