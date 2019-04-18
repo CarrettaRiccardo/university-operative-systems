be9f98bcbfc2b53936b340682a7bfc55ab3ab96b
steve_azzolin <steve.azzolin1@gmail.com>
2019-04-18 23:46:30 +0200

    Aggiunto codice di Test per mailbx

---
+++ project/src/test/mailbox/writer2.c
@@ -0,0 +1,51 @@
+// C Program for Message Queue (Writer Process) 
+#include <stdio.h> 
+#include <sys/ipc.h> 
+#include <sys/msg.h> 
+#include <errno.h>
+
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+  
+// structure for message queue 
+struct mesg_buffer { 
+    long mesg_type; 
+    char mesg_text[10]; 
+    key_t reponse_key;
+} message;
+  
+int main() 
+{ 
+    key_t key; 
+    int msgid; 
+  
+    // ftok to generate unique key 
+    key = ftok("progfile", 65); 
+  
+    // msgget creates a message queue 
+    // and returns identifier 
+    msgid = msgget(key, 0666 | IPC_CREAT); 
+    message.mesg_type = 1; 
+
+    if(msgid == -1){
+    	perror("Error ");
+    	//printf("Errore 1 %d \n",errno);
+    }
+
+    printf("Write Data : "); 
+    gets(message.mesg_text); 
+  
+    // msgsnd to send message 
+    int ret = msgsnd(msgid, &message, sizeof(message), 0); 
+
+    if(ret == -1){
+    	printf("Errore 2 %s \n",strerror(errno));
+    }
+  
+    // display the message 
+    printf("Data send is : %s \n", message.mesg_text); 
+  
+    return 0; 
+}
