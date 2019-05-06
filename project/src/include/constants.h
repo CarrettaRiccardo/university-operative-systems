c3be95cce1e42574a3d5b1651c4835e36ec7585b
Riccardo Carretta <riccardo.carretta99@gmail.com>
2019-05-06 13:56:03 +0200

    File constants

---
+++ project/src/include/constants.h
@@ -0,0 +1,14 @@
+#define SWITCH_ON "switch_on"
+#define SWITCH_OFF "switch_off"
+#define SET_TIME_DELAY "set_delay"
+#define SET_TIME_BEGIN "set_begin"
+#define SET_TIME_END "set_end"
+#define SET_TEMPERATURE "set_temperature"
+#define SET_PERC_FILLED "set_filled"
+
+#define INFO_REQUEST "request_info"
+#define INFO_RESPONSE "response_info"
+
+#define MSG_DELETE "delete"
+#define MSG_LINK "link"
+#define MSG_SWITCH "switch"
