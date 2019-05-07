8f0c254494efb4391f3bd5f5ba596b9e5d74b40b
Matteo Destro <matteo.est@gmail.com>
2019-05-07 17:13:21 +0200

    fix

--- project/src/bulb/bulb.c
+++ project/src/bulb/bulb.c
@@ -25,19 +25,19 @@ int main(int argc, char **argv) {
             if (msg.to == -1) continue;  // Messaggio da ignorare (per sessione diversa/altri casi)
 
             if (strcmp(msg.text, MSG_DELETE_REQUEST) == 0) {
-                message_t msg = buildDieResponse(msg.sender);
-                sendMessage(&msg);
+                message_t m = buildDieResponse(msg.sender);
+                sendMessage(&m);
                 exit(0);
             } else if (strcmp(msg.text, INFO_REQUEST) == 0) {
                 unsigned long work_time = time(NULL) - start_time;
-                message_t msg = buildInfoResponseBulb(id, stato, msg.sender, "Bulb", work_time);
-                sendMessage(&msg);
+                message_t m = buildInfoResponseBulb(id, stato, msg.sender, "Bulb", work_time);
+                sendMessage(&m);
             } else if (strcmp(msg.text, MSG_TRANSLATE) == 0) {
                 message_t m = buildTranslateResponse(id, msg.value1, msg.sender);
-                sendMessage(&msg);
+                sendMessage(&m);
             } else if (strcmp(msg.text, MSG_LIST) == 0) {  // Caso base per la LIST. value5 = 1 per indicare fine albero
                 message_t m = buildListResponse(msg.sender, "Bulb", stato, msg.value1, 1, id);
-                sendMessage(&msg);
+                sendMessage(&m);
             }
         }
     }
