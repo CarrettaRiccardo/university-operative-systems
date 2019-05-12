e04de51aff31eeac18c5e7ce85786c4517a35774
Riccardo Carretta <riccardo.carretta99@gmail.com>
2019-05-12 13:20:43 +0200

    lavoro su timer

--- project/src/timer/timer.c
+++ project/src/timer/timer.c
@@ -1,9 +1,137 @@
-/*
-TODO: Remove not-allowed libraries
-*/
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "../include/ipc.h"
+#include "../include/list.h"
+#include "../include/utils.h"
+
+char *base_dir;
+int id;
+list_t children;
+
+long begin = 0;  //momento temporale di attivazione
+long end = 0;    //momento temporale di disattivazione
+
+//Override del metodo in IPC.C per il componente Timer
+message_t buildInfoResponseTimer(int sender);
 
 int main(int argc, char **argv) {
+    base_dir = extractBaseDir(argv[0]);
+    id = atoi(argv[1]);
+    children = listInit();
+    if (argc > 2) {  //  Clone del timer
+        int to_clone_pid = atol(argv[2]);
+        message_t request = buildGetChildRequest(to_clone_pid);
+        message_t response;
+        int child_pid;
+        sendMessage(&request);
+        // Linka tutti il figlio del timer clonato a sè stesso
+        receiveMessage(&response);
+        child_pid = response.vals[GET_CHILDREN_VAL_ID];
+        if (child_pid != -1) {
+            doLink(children, child_pid, getppid(), base_dir);
+            message_t ack = buildResponse(to_clone_pid, -1);
+            sendMessage(&ack);
+        }
+    }
+
+    while (1) {
+        message_t msg;
+        if (receiveMessage(&msg) == -1) {
+            perror("TIMER: Error receive message");
+        } else {
+            if (msg.type == INFO_MSG_TYPE) {
+                message_t m = buildInfoResponseTimer(msg.sender);
+                sendMessage(&m);
+            } else if (msg.type == SWITCH_MSG_TYPE) {
+                // apertura/chiusura
+            } else if (msg.type == LINK_MSG_TYPE) {
+                if (listCount(children) < 1)// se non ha figlio
+                    doLink(children, msg.vals[LINK_VAL_PID], msg.sender, base_dir);
+                else
+                    continue;// TODO
+            } else if (msg.type == DELETE_MSG_TYPE) {
+                message_t m = buildDeleteResponse(msg.sender);
+                sendMessage(&m);
+                exit(0);
+            } else if (msg.type == TRANSLATE_MSG_TYPE) {
+                message_t m = buildTranslateResponseControl(msg.sender, id, msg.vals[TRANSLATE_VAL_ID], children);
+                sendMessage(&m);
+            } else if (msg.type == LIST_MSG_TYPE) {  //  Risponde con i propri dati e inoltra la richiesta al figlio
+                message_t m;
+                if (listEmpty(children)) {
+                    m = buildListResponse(msg.sender, id, TIMER, msg.vals[LIST_VAL_LEVEL], 1);
+                    sendMessage(&m);
+                } else {
+                    m = buildListResponse(msg.sender, id, TIMER, msg.vals[LIST_VAL_LEVEL], 0);
+                    sendMessage(&m);
+                    doListControl(msg.sender, children);
+                }
+            } else if (msg.type == CLONE_MSG_TYPE) {
+                int vals[NVAL] = {id, getpid()};
+                message_t m = buildCloneResponse(msg.sender, TIMER, vals);
+                sendMessage(&m);
+            } else if (msg.type == GET_CHILDREN_MSG_TYPE) {
+                //  Invio il figlio al processo che lo richiede
+                message_t m;
+                node_t *p = *children;
+                if (p != NULL) {
+                    m = buildGetChildResponse(msg.sender, p->value);
+                    sendMessage(&m);
+                    message_t resp;
+                    receiveMessage(&resp);
+                }
+                m = buildGetChildResponse(msg.sender, -1);
+                sendMessage(&m);
+            } else if (msg.type == DIE_MESG_TYPE) {
+                //  Rimuovo il mittente di questo messaggio dalla lista dei miei figli
+                listRemove(children, msg.sender);
+            }
+        }
+    }
+
     return 0;
+}
+
+//Stato = Override <-> è presente nel TIMER ?? --  TODO
+message_t buildInfoResponseTimer(int sender) {
+    node_t *p = *children;
+    short stato_figlio = -1;
+
+    while (p != NULL) {
+        int id_processo = p->value;
+        message_t request = buildInfoRequest(id_processo);
+        message_t response;
+        
+        if (sendMessage(&request) == -1) {
+            perror("Error get pid by id request");
+        } else if (receiveMessage(&response) == -1) {
+            perror("Error get pid by id response");
+        } else {
+            if(response.type != INFO_MSG_TYPE){
+                message_t busy = buildBusyResponse(response.sender);
+                sendMessage(&busy);
+                continue;//per evitare annidamento di if e parentesi, faccio ripartire il ciclo sullo stesso figlio. Letto un messaggio non pertinente
+            }
+
+            if(stato_figlio != -1 && stato_figlio != response.vals[INFO_VAL_STATE]){
+                message_t ret = buildInfoResponse(sender,TIMER);
+                ret.vals[INFO_VAL_STATE] = 3; //stato di override
+                return ret;
+            }
+            else{
+                stato_figlio = response.vals[INFO_VAL_STATE];
+            }
+        }
+        // fa terminare il ciclo (saltato in caso legga un messaggio errato per il figlio)
+        p = NULL;
+    }
+    message_t ret = buildInfoResponse(sender, TIMER);
+    ret.vals[INFO_VAL_STATE] = stato_figlio; //stato di override
+    return ret;
 }
 
 /*
